bd06ff6bd43a06cff2764e8af7db50e30f6c7322
alfacad786 <alfa_cad786@yahoo.com>
2023-12-11 22:06:37 +0530

    add delete route when user delete also user data(user all payment)is delet

--- routes/admin.js
+++ routes/admin.js
@@ -65,80 +65,90 @@ router.get("/log/porter/", async (req, res) => {
 
 // SERCH profile  by Admin =============================================
 
-router.post("/:id/profile/", async (req, res) => {
+router.post("/:userName/profile/", async (req, res) => {
   // let data = req.params;
-  let { id } = req.params;
-  let don = await admindetail.findById(id);
-  const data = [don];
+  // let { id } = req.params;
+  // let don = await admindetail.findById(id);
+  // const data = [don];
+  let { userName } = req.params;
+  let don = await admindetail.findOne({ userName: userName });
+  const data = [don]
 
   if (!don) {
-    res.render("adminsearchformalert.ejs", { don });
+    res.render("adminsearchformalert.ejs", { don,data });
   } else {
-    res.render("adminProfile.ejs", { data, don });
+    res.render("adminProfile.ejs", { data,don });
   }
 
-  console.log(id, don, data);
+  console.log("don="+ don,"data=", data);
 
   // res.send(don);
 });
 // back userportal request ========================================
 
-router.post("/back/:id/", async (req, res) => {
-  let { id } = req.params;
-  let don = await admindetail.findById(id);
+router.post("/back/:userName/", async (req, res) => {
+  // let { id } = req.params;
+  // let don = await admindetail.findById(id);
+  // const data = [don];
+  let { userName } = req.params;
+  let don = await admindetail.findOne({ userName: userName });
   const data = [don];
 
-  res.render("adminportel.ejs", {id, don, data });
-  console.log({ id, don });
+  res.render("adminportel.ejs", { don, data });
+  console.log({  don });
   // console.log("/trust/userportal/",{userName},{ data });
 });
 // search data  request ========================================
-router.post("/:id/search/", async (req, res) => {
+router.post("/:userName/search/", async (req, res) => {
   let search = req.body.search;
   let Adminlist = { search: "Adminlist" };
   let userlist = { search: "userlist" };
   let { value } = req.params;
-  let { id } = req.params;
-  let don = await admindetail.findById(id);
+  // let { id } = req.params;
+  // let don = await admindetail.findById(id);
+  let { userName } = req.params;
+  let don = await admindetail.findOne({ userName: userName });
+  const data = [don];
   let admin = await admindetail.find();
-  let user = await userdetail.find();
+  let alluser = await userdetail.find();
   let fund = await user1.find();
 
   if (search === "Adminlist") {
     console.log("adminlist");
-    res.render("adminlist.ejs", { admin, don });
+    res.render("adminlist.ejs", {don,data, admin });
   } else if (search === "userlist") {
-    res.render("admin-userlist copy.ejs", { user, don });
+    res.render("admin-userlist copy.ejs", {fund, alluser, don });
   } else if (search === "totalpayment") {
     res.render("adminfund copy.ejs", { fund, don });
     console.log("totalpayment", fund, don);
     // res.render("searchformalert.ejs", { don });
   }
 
-  console.log(id,"admin :"+don);
+  console.log("admin :",don,admin);
 });
 
 
 // delet userprofile and relative doc data  request ========================================
-router.delete("/user/:id", async (req, res) => {
-  let { id } = req.params;
-  let deletuser = await userdetail.findByIdAndDelete(id);
-  let  userId  = deletuser._id;
-  let user = await userdetail.find();
-  // let deletfund = await user1.findByIdAndDelete(id);
-  // let  userId  = delet.userId  
-  let don = await user1.find ({ userId : id  });
-  for (let i = 0; i < user1.length; i++) {
-    let don = await user1.findOneAndDelete ({ userId : id  });
-    console.log(don, "ok delet");
+router.delete("/user/:id/:userName/", async (req, res) => {
+
+  let { userName } = req.params;
+  let don = await admindetail.findOne({ userName: userName });
+  //-----------------------------------------------------
+  let { id } = req.params; 
+  let mainuser = await userdetail.findById(id);
+  let  userId  = mainuser._id;   
+  let user = await user1.find({ userId : id  });
+  for (let i = 0 ; i < user.length; i++) {
+    let user = await user1.findOneAndDelete ({ userId : id  });
+   
+    console.log(user, "ok delet");
   }
-  // let fund = await user1.find();
-  // // res.redirect("/back/:id/");
+  let deletuser = await userdetail.findByIdAndDelete(id); 
+  let alluser = await userdetail.find(); 
 
-  res.render("admin-userlist copy.ejs", { user, don });
+  res.render("admin-userlist copy.ejs", {don,alluser, don });
 
-  console.log(id,deletuser,userId,don,  "delet");
- 
+  console.log("userName=",userName,"don=",don,"id=",id,"deletuser=",deletuser,"userId=",userId,"alluser=",alluser,  "delet");
 });
 
 // delet user donetion data  request ========================================
